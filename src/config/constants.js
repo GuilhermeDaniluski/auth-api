217eaba110ca836eeb601a3b86abc244c968b26b
Guilherme Daniluski <guilhermedaniluski@gmail.com>
2020-06-18 21:06:54 -0300

    Dockerization Complete

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-        mongoUri: 'mongodb://localhost:27017/raspserver'
+        mongoUri: 'mongodb://mongo:27017/raspserver'
     },
     auth:{
         jwtKey:'43c3b006ec7526feabea9697c487e414'
--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose')
 const { db } = require('./constants')
 
+
+
 mongoose.connect(db.mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: true
-})
+    useFindAndModify: true,
+    useUnifiedTopology: true
+}, function(err, db) {
+        if (err) {
+            console.log('Unable to connect to the server. Please start the server. Error:', err);
+            process.exit(0)
+        } else {
+            console.log('Connected to Server successfully!');
+        }
+    });
